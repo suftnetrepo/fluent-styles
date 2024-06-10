7641d6123ab3bf0adba74c976a7544bf96352c6f
Abel Aghorighor <52415319+aaghorighor@users.noreply.github.com>
2024-06-10 21:51:35 +0100

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'react-native',
+    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    },
+    transformIgnorePatterns: [
+        'node_modules/(?!(react-native|@react-native|@react-native-community)/)',
+    ],
+};
---
+++ src/assets/styled/index.js
@@ -0,0 +1,50 @@
+import React, { forwardRef } from "react";
+
+const styled = (Component, { base, variants }) => {
+  return forwardRef((props, ref) => {
+    const styles = { ...base };
+    const options = props;
+
+    Object.keys(variants || {}).forEach((category) => {
+      const variantSelected = options[category];
+      const variantValue = variants[category];
+
+      if (typeof variantValue === "function") {
+        const style = variantValue(variantSelected, options);
+        if (style) {
+          Object.assign(styles, style);
+        }
+      } else if (
+        variantSelected &&
+        typeof variantSelected === "string" &&
+        variantSelected.includes(".")
+      ) {
+        const value = getNestedVariant(variantValue, variantSelected);
+        if (value) {
+          Object.assign(
+            styles,
+            typeof value === "string" ? { [category]: value } : value
+          );
+        }
+      } else if (variantValue && variantValue[variantSelected]) {
+        const value = variantValue[variantSelected];
+        Object.assign(
+          styles,
+          typeof value === "function" ? value(options) : value
+        );
+      }
+    });
+
+    if (props.style) {
+      Object.assign(styles, props.style);
+    }
+
+    return <Component {...props} style={styles} ref={ref} />;
+  });
+};
+
+function getNestedVariant(object, path) {
+  return path.split(".").reduce((acc, part) => acc && acc[part], object);
+}
+
+export default styled;
